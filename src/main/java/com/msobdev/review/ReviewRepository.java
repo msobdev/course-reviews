c41b52fcc80edbed193545581e623602b8cd69db
msobdev <msobdev@gmail.com>
2017-07-21 22:48:28 +0200

    Paging

--- src/main/java/com/msobdev/review/ReviewRepository.java
+++ src/main/java/com/msobdev/review/ReviewRepository.java
@@ -1,10 +1,15 @@
 package com.msobdev.review;
 
+import com.msobdev.course.Course;
+import org.springframework.data.domain.Page;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 /**
  * Created by sob1 on 20.07.2017.
  */
-public interface ReviewRepository extends CrudRepository<Review, Long> {
+public interface ReviewRepository extends PagingAndSortingRepository<Review, Long> {
+
 
 }
