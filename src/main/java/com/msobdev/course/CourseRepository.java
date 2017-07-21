ce48d032314f4a2132b937339d7fedeaa028e24d
msobdev <msobdev@gmail.com>
2017-07-21 22:49:06 +0200

    Paging

--- src/main/java/com/msobdev/course/CourseRepository.java
+++ src/main/java/com/msobdev/course/CourseRepository.java
@@ -2,7 +2,6 @@ package com.msobdev.course;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
--- src/main/java/com/msobdev/review/ReviewRepository.java
+++ src/main/java/com/msobdev/review/ReviewRepository.java
@@ -1,10 +1,6 @@
 package com.msobdev.review;
 
-import com.msobdev.course.Course;
-import org.springframework.data.domain.Page;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
 
 /**
  * Created by sob1 on 20.07.2017.
