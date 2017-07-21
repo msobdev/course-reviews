1974d1bf3c58d925d610f392c8aac1ac432e3e36
msobdev <msobdev@gmail.com>
2017-07-21 23:10:59 +0200

    Versioning, e-tags

--- src/main/java/com/msobdev/core/BaseEntity.java
+++ src/main/java/com/msobdev/core/BaseEntity.java
@@ -1,10 +1,7 @@
 package com.msobdev.core;
 
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 
 /**
  * Created by sob1 on 21.07.2017.
@@ -15,6 +12,8 @@ public abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private final Long id;
+    @Version
+    private Long version;
 
     protected BaseEntity() {
         id = null;
